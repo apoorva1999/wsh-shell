8d7aa8c10ce98b00bd2976db8e91e0714a39a89b
Apoorva Mittal <apoorva@rockhopper-08.cs.wisc.edu>
2024-10-02 20:46:50 -0500

    added exit built-in

--- solution/wsh.c
+++ solution/wsh.c
@@ -1,7 +1,35 @@
 #include<stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <assert.h>
+#include<string.h>
+#define EXIT "exit\n"
+void exitF(void) {
+    exit(0);
+}
+int main(int argc, char *argv[]) {
+    char* bashscript=NULL;
+    if(argc==2) {
+            bashscript = argv[1];
+    }
+    else if(argc>2) return -1;
 
+    assert(bashscript == NULL);
 
-int main(void) {
-    printf("hello world\n");
-    return 0;
+   char *buffer = (char *) malloc(sizeof(char));
+   size_t bufSize = 8;
+   
+   do {
+        printf("wsh> ");
+        if(getline(&buffer, &bufSize, stdin) != EOF)
+        {
+        if(strcmp(buffer, EXIT) == 0) {
+            exitF();
+        }}
+        else break;
+
+   } while(getline(&buffer, &bufSize, stdin) != EOF);
+
+   free(buffer);
 }
