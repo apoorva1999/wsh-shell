ae39bca523eae3cc89855bbe12c7cbcb3ad99f55
apoorva <apletters001@gmail.com>
2024-10-08 18:59:27 -0500

    Added code for redirection, utility function for perror and arguments

--- solution/wsh.c
+++ solution/wsh.c
@@ -5,6 +5,9 @@
 #include <unistd.h>
 #include <dirent.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdarg.h>
 
 
 #define EXIT "exit"
@@ -19,8 +22,144 @@
 #define VARS "vars"
 #define HISTORY_SET "set"
 #define LOCAL "local"
+#define REDIR_INPUT "<"
+#define REDIR_OUTPUT ">"
+#define APPEND_OUTPUT ">>"
+#define REDIR_OUTPUT_ERROR "&>"
+#define APPEND_OUTPUT_ERROR "&>>"
+
+  
+int stdout_fd = -1;
+int stderr_fd = -1;
+int stdin_fd = -1;
+int file_fd = -1;
 
 // extern char **environ;
+char* redirections[] = {"&>>", "&>", ">>", ">" ,"<"};
+void printS(char *name, const char *val)
+{
+    printf("[%s : %s]\n", name, val);
+}
+
+void log_error(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+
+    vfprintf(stderr, fmt, args);
+    fprintf(stderr, ": %s\n", strerror(errno));
+
+    va_end(args);
+}
+
+int save_fd(int oldfd, int* newfd, char*fd_name) {
+    *newfd = dup(oldfd);
+    if(*newfd < 0) {
+        log_error("Failed to duplicate %s", fd_name);
+        return 1;
+    }
+    return 0;
+} 
+
+int copy_fd(int fd1, int fd2, char*fd1_name, char*fd2_name) {
+    if(dup2(fd1, fd2) < 0) {
+        log_error("Failed to duplicate %s to %s", fd1_name, fd2_name);
+        return 1;
+    }
+    return 0;
+}
+
+int append_output_error(char* input) {
+    // &>>
+    input+=3;
+
+    if(save_fd(STDOUT_FILENO, &stdout_fd, "stdout_fd")) return 1;
+    if(save_fd(STDERR_FILENO, &stderr_fd, "stderr_fd")) return 1;
+
+    file_fd = open(input, O_WRONLY | O_CREAT | O_APPEND, 0644);
+    if(file_fd < 0) {
+        log_error("Failed to open file %s", input);
+        return 1;
+    }
+
+    if(copy_fd(file_fd, STDOUT_FILENO, "file_fd", "stdout_fd"))  return 1;
+    if(copy_fd(file_fd, STDERR_FILENO, "file_fd", "stderr_fd"))  return 1;
+
+    return 0;
+}
+
+int redirect_output_error(char* input) {
+    // &>
+    input+=2;
+
+    if(save_fd(STDOUT_FILENO, &stdout_fd, "stdout_fd")) return 1;
+    if(save_fd(STDERR_FILENO, &stderr_fd, "stderr_fd")) return 1;
+
+
+    file_fd = open(input, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if(file_fd < 0) {
+        perror("Failed to open file");
+        return 1;
+    }
+
+    if(copy_fd(file_fd, STDOUT_FILENO, "file_fd", "stdout_fd"))  return 1;
+    if(copy_fd(file_fd, STDERR_FILENO, "file_fd", "stderr_fd"))  return 1;
+
+    return 0;
+}
+
+
+int append_output(char* input) {
+    // >>
+    input+=2;
+
+    if(save_fd(STDOUT_FILENO, &stdout_fd, "stdout_fd")) return 1;
+
+    file_fd = open(input, O_WRONLY | O_CREAT | O_APPEND , 0644);
+    if(file_fd < 0) {
+        perror("Failed to open file");
+        return 1;
+    }
+
+    if(copy_fd(file_fd, STDOUT_FILENO, "file_fd", "stdout_fd"))  return 1;
+    
+    return 0;
+}
+
+
+int redirect_output(char* input) {
+    // >
+    input+=1;
+
+    if(save_fd(STDOUT_FILENO, &stdout_fd, "stdout_fd")) return 1;
+
+    file_fd = open(input, O_WRONLY | O_CREAT | O_TRUNC , 0644);
+    if(file_fd < 0) {
+        perror("Failed to open file");
+        return 1;
+    }
+
+    if(copy_fd(file_fd, STDOUT_FILENO, "file_fd", "stdout_fd"))  return 1;
+    
+    return 0;
+}
+
+int redirect_input(char* input) {
+    // <
+    input+=1;
+    if(save_fd(STDIN_FILENO, &stdin_fd, "stdin_fd")) return 1;
+
+    file_fd = open(input, O_RDONLY | O_CREAT , 0644);
+    if(file_fd < 0) {
+        perror("Failed to open file");
+        return 1;
+    }
+
+    if(copy_fd(file_fd, STDIN_FILENO, "file_fd", "stdout_fd"))  return 1;
+    
+    return 0;
+}
+
+int (*redirection_functions[])(char*) = {append_output_error, redirect_output_error, append_output, redirect_output, redirect_input};
 
 struct Node
 {
@@ -37,10 +176,7 @@ typedef struct
 
 int exit_value = 0;
 
-void printS(char *name, const char *val)
-{
-    printf("[%s : %s]\n", name, val);
-}
+
 
 void printD(char *name, int val)
 {
@@ -356,6 +492,7 @@ void historyF(History *history)
             }
             const char *nthHistory = getHistory(history, n);
             if (nthHistory)
+                //TODO
                 printS("executing....", nthHistory);
         }
     }
@@ -396,6 +533,35 @@ void exportF(void)
     exit_value = setenv(name, value, 1);
 }
 
+void resetFDs(void) {
+    if(file_fd!=-1) {
+       if(close(file_fd) < 0) {
+            exit_value = 1;
+       }
+    }
+
+    if(stderr_fd!=-1) {
+        //todo: check -1
+        dup2(stderr_fd, STDERR_FILENO);
+        close(stderr_fd);
+    }
+
+    if(stdout_fd!=-1) {
+        dup2(stdout_fd, STDOUT_FILENO);
+        close(stdout_fd);
+    }
+
+    if(stdout_fd!=-1) {
+        dup2(stdin_fd, STDIN_FILENO);
+        close(stdin_fd);
+    }
+
+    stdout_fd = -1;
+    stderr_fd = -1;
+    file_fd = -1;
+    stdin_fd = -1;
+
+}
 int addArg(char ***args, char *arg, int *argc)
 {
     if ((*args = realloc(*args, sizeof(char *) * (*argc + 1))) != NULL)
@@ -441,6 +607,8 @@ int executeCommand(char *command, char *input)
     char *dir = strtok(PATH, COLON_SIGN_DELIMETER);
     char *newPath = NULL;
     exit_value = 1;
+    // TODO : test other paths like env
+    // test what happes when we passs echo hello world
     while (dir)
     {
 
@@ -450,7 +618,6 @@ int executeCommand(char *command, char *input)
             strcat(newPath, dir);
             strcat(newPath, "/");
             strcat(newPath, command);
-
             if ((exit_value = access(newPath, X_OK)) == 0)
             {
                 pid_t cpid = fork();
@@ -464,11 +631,12 @@ int executeCommand(char *command, char *input)
                 {
                     if (execv(newPath, args) == -1)
                     {
+
                         perror("execv failed");
                         exit_value = 1;
                         break;
                     }
-                    perror("child process failed\n");
+                    perror("child process failed");
                     exit_value = 1;
                     break;
                 }
@@ -506,13 +674,24 @@ int main(int argc, char *argv[])
     LocalVars *localVars = createLocalVars();
     char *input = NULL;
 
-    printS("PATH", getenv("PATH"));
     setenv("PATH", "/bin", 1);
 
     while (printf("wsh> ") && getString(&buffer, stdin) != EOF)
     {
         input = realloc(input, sizeof(char) * (strlen(buffer) + 1));
         strcpy(input, buffer);
+
+        
+        for(int i=0;i<5;i++) {
+            char* file_path = strstr(input, redirections[i]);
+            if(file_path != NULL) {
+                char* file_path_copy = strdup(file_path);
+                file_path[0] = '\0';
+                redirection_functions[i](file_path_copy);   
+                break;
+            }
+        }
+
         char *command = strtok(buffer, SPACE_DELIMETER);
         if (strcmp(command, EXIT) == 0)
         {
@@ -547,6 +726,8 @@ int main(int argc, char *argv[])
             saveCommandToHistory(history, input);
             executeCommand(command, input);
         }
+
+        resetFDs();
         free(buffer);
         buffer = NULL;
     }
@@ -556,3 +737,4 @@ int main(int argc, char *argv[])
     freeLocalVars(localVars);
     exit(0);
 }
+
